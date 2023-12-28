312c57aada063d6a3637798c6d10c15da80d2424
harun <harunbahiir@gmail.com>
2023-12-28 19:32:48 +0300

    Loading page
    Loading page xml
    after the loading page it is redirected to login page

--- app/src/main/java/com/example/pawrescue/MainActivity.java
+++ app/src/main/java/com/example/pawrescue/MainActivity.java
@@ -1,49 +1,52 @@
 package com.example.pawrescue;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Handler;
 import android.view.View;
-
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.example.pawrescue.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
-import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
+
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setTheme(R.style.LoadingTheme);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        WebView webViewBackground = findViewById(R.id.webViewBackground);
-        webViewBackground.loadUrl("file:///android_res/drawable/animated_background.gif");
-        setSupportActionBar(binding.toolbar);
+        setContentView(R.layout.activity_loading);
+       // WebView webViewBackground = findViewById(R.id.webViewBackground);
+       // webViewBackground.loadUrl("file:///android_res/drawable/animated_background.gif");
+       // setSupportActionBar(binding.textView3);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Transition to the login page
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish(); // Finish the current activity to prevent going back to the loading page
+            }
+        }, 2000);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+
+        binding.imageView5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
+                        .setAnchorView(R.id.imageView5)
                         .setAction("Action", null).show();
             }
         });
@@ -71,10 +74,6 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+
+
 }
