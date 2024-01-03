c62cc6cb0fe99a01c0a883499653ec44850a6946
Zekeriyaozden <zekeriyaozden@icloud.com>
2024-01-03 18:55:46 +0300

    Emergency

--- app/src/main/java/com/example/pawrescue/EmergencyActivity.java
+++ app/src/main/java/com/example/pawrescue/EmergencyActivity.java
@@ -1,6 +1,9 @@
 package com.example.pawrescue;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -10,6 +13,7 @@ import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -29,6 +33,8 @@ import java.net.URL;
 
 public class EmergencyActivity extends AppCompatActivity {
     private Spinner countrySpinner;
+
+    ImageView iv1,iv2,iv3;
     private TextView text1,text2,text3;
     private ArrayAdapter<CharSequence> adapterCountry;
     private Button bt;
@@ -48,9 +54,13 @@ public class EmergencyActivity extends AppCompatActivity {
                 getWindow().setNavigationBarColor(getResources().getColor(android.R.color.transparent, getTheme()));
             }
         }
+
+
         setContentView(R.layout.activity_emergency);
-
-
+        // Önce Resources objesini al
+        Resources resources = getResources();
+        // Ardından string-array'i al
+        String[] codeArray = resources.getStringArray(R.array.countryCode_array);
         bt = findViewById(R.id.button);
         text1 = findViewById(R.id.editText1);
         text2 = findViewById(R.id.editText2);
@@ -60,17 +70,75 @@ public class EmergencyActivity extends AppCompatActivity {
         adapterCountry.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         countrySpinner.setAdapter(adapterCountry);
 
+        bt.setVisibility(View.INVISIBLE);
+        iv1 = findViewById(R.id.callButton1);
+        iv2 = findViewById(R.id.callButton2);
+        iv3 = findViewById(R.id.callButton3);
+        iv1.setVisibility(View.INVISIBLE);
+        iv2.setVisibility(View.INVISIBLE);
+        iv3.setVisibility(View.INVISIBLE);
+
+
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 apiCall();
+
+            }
+        });
+
+        iv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = text1.getText().toString(); // Burayı kendi telefon numaranızla değiştirin
+
+                // Telefon uygulamasını başlatmak için bir Intent oluşturun
+                Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
+
+                // Telefon uygulamasını başlatın
+                startActivity(dialIntent);
+            }
+        });
+
+        iv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = text2.getText().toString(); // Burayı kendi telefon numaranızla değiştirin
+
+                // Telefon uygulamasını başlatmak için bir Intent oluşturun
+                Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
+
+                // Telefon uygulamasını başlatın
+                startActivity(dialIntent);
+            }
+        });
+
+        iv3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = text3.getText().toString(); // Burayı kendi telefon numaranızla değiştirin
+
+                // Telefon uygulamasını başlatmak için bir Intent oluşturun
+                Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
+
+                // Telefon uygulamasını başlatın
+                startActivity(dialIntent);
             }
         });
 
         countrySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                selectedCountry = parent.getItemAtPosition(position).toString();
+                if(position>0){
+                    selectedCountry = codeArray[position].toString();
+                    bt.setVisibility(View.VISIBLE);
+                }else{
+                    bt.setVisibility(View.INVISIBLE);
+                    iv1.setVisibility(View.INVISIBLE);
+                    iv2.setVisibility(View.INVISIBLE);
+                    iv3.setVisibility(View.INVISIBLE);
+                }
+
             }
 
             @Override
@@ -94,7 +162,6 @@ public class EmergencyActivity extends AppCompatActivity {
             @Override
             protected String doInBackground(Void... params) {
                 try {
-                    selectedCountry = "TR";
                     URL url = new URL("https://emergencynumberapi.com/api/country/" + selectedCountry);
                     HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                     try {
@@ -137,16 +204,16 @@ public class EmergencyActivity extends AppCompatActivity {
 
 
 
-                        String isoCode = countryObject.getString("ISOCode");
-                        String isoNumeric = countryObject.getString("ISONumeric");
-
 
                         // Similarly, you can parse other information like fire, police, etc.
 
                         // Now, update your text views with the obtained information
-                        text1.setText(ambulance);
-                        text2.setText(fire);
-                        text3.setText(police);
+                        text1.setText(police);
+                        text2.setText(ambulance);
+                        text3.setText(fire);
+                        iv1.setVisibility(View.VISIBLE);
+                        iv2.setVisibility(View.VISIBLE);
+                        iv3.setVisibility(View.VISIBLE);
                     } catch (JSONException e) {
                         e.printStackTrace();
                         text1.setText("JSON parsing failed");
