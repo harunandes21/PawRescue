da660a337704db1a72b4df95ba4965ecf845bedc
0x1c3N <bthnstars@gmail.com>
2024-01-03 22:21:30 +0300

    PetFinderAPIFinish

--- app/src/main/java/com/example/pawrescue/AdoptionActivity.java
+++ app/src/main/java/com/example/pawrescue/AdoptionActivity.java
@@ -1,5 +1,6 @@
 package com.example.pawrescue;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.ContentValues;
 import android.content.Intent;
@@ -8,13 +9,16 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import com.squareup.picasso.Picasso;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,6 +34,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class AdoptionActivity extends AppCompatActivity {
     static final Uri CONTENT_URI_USER = UserProvider.CONTENT_URI_USER;
@@ -38,13 +43,20 @@ public class AdoptionActivity extends AppCompatActivity {
     private Spinner locationSpinner,speciesSpinner,ageSpinner,healthStatusSpinner;
     private String location,species,age,healthStatus;
     private ArrayAdapter<CharSequence> adapterLocation,adapterSpecies,adapterAge, gender;
+    Button showLeft, showRight;
     public String jsonString;
     public String apiUrl_type;
     public String apiUrl_age;
     public String apiUrl_gender;
     public String apiUrl_location;
     public int animalIndex = 0;
+    public ArrayList<String> petURL = new ArrayList<>();
+    public ArrayList<String> petName = new ArrayList<>();
+    public ArrayList<String> petStatus = new ArrayList<>();
+    public ArrayList<String> petCity = new ArrayList<>();
     View searchButton;
+    ImageView petPhoto;
+    TextView petInfo;
     Intent intent;
     User loggedInUser;
     @Override
@@ -82,6 +94,14 @@ public class AdoptionActivity extends AppCompatActivity {
         gender.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         healthStatusSpinner.setAdapter(gender);
         searchButton = findViewById(R.id.imageSearchButton);
+        showLeft = findViewById(R.id.buttonLeft);
+        showLeft.setVisibility(View.INVISIBLE);
+        showRight = findViewById(R.id.buttonRight);
+        showRight.setVisibility(View.INVISIBLE);
+        petPhoto = findViewById(R.id.imageView2);
+        petPhoto.setVisibility(View.INVISIBLE);
+        petInfo = findViewById(R.id.textView);
+        petInfo.setVisibility(View.INVISIBLE);
         intent = getIntent();
         loggedInUser = (User) intent.getSerializableExtra("user");
 
@@ -101,6 +121,32 @@ public class AdoptionActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 new FetchDataFromApi().execute();
+            }
+        });
+
+        showLeft.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                if (animalIndex == 0) {
+                    showAlertDialogAdopt("Error", "You are already viewing the first pet");
+                } else {
+                    Picasso.get().load(petURL.get(--animalIndex)).into(petPhoto);
+                    petInfo.setText("Name = " + petName.get(animalIndex) + "\nStatus = " + petStatus.get(animalIndex) + "\nCity = " + petCity.get(animalIndex));
+                }
+            }
+        });
+
+        showRight.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                if (animalIndex == petURL.size() - 1) {
+                    showAlertDialogAdopt("Error", "You are already viewing the last pet");
+                } else {
+                    Picasso.get().load(petURL.get(++animalIndex)).into(petPhoto);
+                    petInfo.setText("Name = " + petName.get(animalIndex) + "\nStatus = " + petStatus.get(animalIndex) + "\nCity = " + petCity.get(animalIndex));
+                }
             }
         });
 
@@ -170,6 +216,16 @@ public class AdoptionActivity extends AppCompatActivity {
         dialog.show();
     }
 
+    private void showAlertDialogAdopt(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
     // AsyncTask to perform network operation in the background
     private class FetchDataFromApi extends AsyncTask<Void, Void, String> {
 
@@ -194,6 +250,7 @@ public class AdoptionActivity extends AppCompatActivity {
             }
         }
 
+        @SuppressLint("SetTextI18n")
         @Override
         protected void onPostExecute(String result) {
             // Update UI with the result
@@ -201,6 +258,12 @@ public class AdoptionActivity extends AppCompatActivity {
             progressDialog.dismiss(); // Dismiss the progress dialog
             try {
                 parseApiDataToScreen();
+                showLeft.setVisibility(View.VISIBLE);
+                showRight.setVisibility(View.VISIBLE);
+                petPhoto.setVisibility(View.VISIBLE);
+                petInfo.setVisibility(View.VISIBLE);
+                Picasso.get().load(petURL.get(0)).into(petPhoto);
+                petInfo.setText("Name = " + petName.get(0) + "\nStatus = " + petStatus.get(0) + "\nCity = " + petCity.get(0));
             } catch (JSONException e) {
                 throw new RuntimeException(e);
             }
@@ -216,7 +279,7 @@ public class AdoptionActivity extends AppCompatActivity {
         String apiUrl = "https://api.petfinder.com/v2/animals?type="+apiUrl_type+"&age="+apiUrl_age+
                 "&gender="+apiUrl_gender+"&location="+apiUrl_location+"&limit=100&page=1";
 
-        String authToken = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJZYjdHRkpvS0Q0bDQ0N1BkVFRYRjljdXdwakVPRzd0WG13MW9hOHlNVE1pQ2lRYnp1YyIsImp0aSI6IjQzYjg5NmUwMDUxNDVhNGJhNmZlYjcxYTNlZWIxYmExY2MzMDJhMmM0YjZjYmExMDIxMGFjNWM1ZDJjMzgzODY3NjJkNTBjOTdmNjU5NjRhIiwiaWF0IjoxNzA0MzAyNzA4LCJuYmYiOjE3MDQzMDI3MDgsImV4cCI6MTcwNDMwNjMwOCwic3ViIjoiIiwic2NvcGVzIjpbXX0.vaj1KNRUWQFccjScEVrq21GrQQqDNMGbn3SoPcjL5zqsbtTMDyupVLKkYB_V2UHg4H3Y_xiGChVcjp-iv-EQENNpou_Lr251wYdUidfCEaU_8C8cVbnGmvfrmwhLI-G5Lp9ivqM35MNfQu9CxoHdBSXum6YGmIA-q71hBOW7yFUKEK0xwgfJzctBoVjvYMp55XAv8ho1ZalHvD5uUOPujJ25Zns2kWwROyg7-AbvtTXRTXV0srta8zF9qZypJX3b1hp8RQ8-eYdW9paFnQcq6HNNZpTtgSoxTKvI4W466ukH3GpKx0s1alUF8DkJrkZintRzXqC-U-GEiVgZAcIkQQ";
+        String authToken = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJZYjdHRkpvS0Q0bDQ0N1BkVFRYRjljdXdwakVPRzd0WG13MW9hOHlNVE1pQ2lRYnp1YyIsImp0aSI6ImJmN2RjZTBlMGFjNTdmZWQxOWQxOWNkN2U3MWQzYWUxNDk2OGY4ZDE0NjhjZTg2ODUzNTQyNTc4YjUyY2FmN2YzNjVmYmVjNjkwOTI5YjFiIiwiaWF0IjoxNzA0MzA3NDcxLCJuYmYiOjE3MDQzMDc0NzEsImV4cCI6MTcwNDMxMTA3MSwic3ViIjoiIiwic2NvcGVzIjpbXX0.GEKKDQP5kyQZeZU8MP_ZxlhUY--atNtbmOOgLZJAjCu7ZsgBE9IwPTtYkxUk-BeMEEVFG-lzibUxgtg0LNfVIBojwoWEzDq875vceA8cCB3iRdc0d363G7j5i6Rl8LlQuUQ_MBqUxncw_oMjD7atGy6alnqhvo041s5io0A28-p4Gl4wbBjRs6n6TCSr2y0C4cDc_T9ghdNpYw6k85Ay6dhkQZCqbFuN-p3kyMXmdoKTwz21lFrwFzI4w7-PbkBSSU130-BrdiapdzRY1oxZLzNNCwIhma0dt7MLbLMK4OW4_051IEQW5y9K0a4Kl4n7XA4-BsummWtz1-c06SzTEg";
 
         URL url = new URL(apiUrl);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -258,6 +321,12 @@ public class AdoptionActivity extends AppCompatActivity {
 
     public void parseApiDataToScreen() throws JSONException {
 
+        String localpetName, status, city;
+        petURL.clear();
+        petName.clear();
+        petStatus.clear();
+        petCity.clear();
+        animalIndex = 0;
         JSONObject obj = new JSONObject(jsonString);
         JSONArray arr = obj.getJSONArray("animals");
 
@@ -267,14 +336,13 @@ public class AdoptionActivity extends AppCompatActivity {
             JSONArray photos = arr.getJSONObject(i).getJSONArray("photos");
             if (photos.length() == 0) continue;
             String photoLink = photos.getJSONObject(0).getString("full");
-            Log.wtf("BatuhanDebug", photoLink);
+            localpetName = arr.getJSONObject(i).getString("name");
+            status = arr.getJSONObject(i).getString("status");
+            city = arr.getJSONObject(i).getJSONObject("contact").getJSONObject("address").getString("city");
+            petURL.add(photoLink);
+            petName.add(localpetName);
+            petStatus.add(status);
+            petCity.add(city);
         }
-
-
-
-        //String petName = obj.getString("name");
-        //String status = obj.getString("status");
-        //String city = obj.getJSONObject("contact").getJSONObject("address").getString("city");
-
     }
 }
